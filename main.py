39097e1b118efaa6c2b98b0bc30d7d31c7812be5
Dokja123 <danielmalan888@gmail.com>
2025-04-05 22:50:57 +0000

    Update main.py

--- main.py
+++ main.py
@@ -45,10 +45,6 @@ def program():
     check = number.isnumeric()
     lennber = len(number)
     if (check == True):
-        if (lennber < 10 or lennber > 10):
-            delay_print(f"{Fore.RED}Number must be 10 digits\n")
-            program()
-        elif (lennber==10):
             delay_print(f"{Fore.YELLOW}1) Ban number\n")
             delay_print(f"{Fore.YELLOW}2) Information about this number\n")
             option = input(f"{Fore.YELLOW}[+] Choose an option: ")
